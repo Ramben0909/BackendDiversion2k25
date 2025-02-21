cd2f1dc9dbca45735012d004ec5180598e1d6631
Ritam Bhattacharya <ritam07mc@gmail.com>
2025-02-21 22:44:52 +0530

    first commit

---
+++ src/utils/token-manager.ts
@@ -0,0 +1,47 @@
+import dotenv from "dotenv";
+dotenv.config();
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { COOKIE_NAME } from "./constants.js";
+
+export const createToken = (id: string, email: string, expiresIn: string | number) => {
+  const payload = { id, email };
+
+  if (!process.env.JWT_SECRET) {
+    throw new Error("JWT_SECRET is not defined in the environment variables.");
+  }
+
+  const token = jwt.sign(payload, process.env.JWT_SECRET as jwt.Secret, { expiresIn } as jwt.SignOptions);
+  return token;
+};
+
+export const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
+  const token = req.signedCookies[COOKIE_NAME];
+  if (!token || token.trim() === "") {
+    return res.status(401).json({ message: "Token Not Received" });
+  }
+
+  if (!process.env.JWT_SECRET) {
+    return res.status(500).json({ message: "JWT_SECRET is not set on the server" });
+  }
+
+  interface JwtPayload {
+    id: string;
+    email: string;
+  }
+
+  interface JwtVerifyCallback {
+    (err: jwt.VerifyErrors | null, decoded: JwtPayload | undefined): void;
+  }
+
+  jwt.verify(token, process.env.JWT_SECRET as jwt.Secret, {}, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({ message: "Token Expired" });
+    } else if (typeof decoded === 'object' && decoded !== null) {
+      res.locals.jwtData = decoded as JwtPayload;
+      return next();
+    } else {
+      return res.status(401).json({ message: "Invalid Token" });
+    }
+  });
+};
